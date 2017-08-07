0d95d769e186ed3e51554f155a24a16fbaf845c2
boisvert42 <boisvert42@gmail.com>
2017-08-07 11:48:12 -0700

    Update 0806_animal_inauguration.py

--- 2017/0806_animal_inauguration.py
+++ 2017/0806_animal_inauguration.py
@@ -6,7 +6,7 @@ http://www.npr.org/2017/08/06/541834096/sunday-puzzle-a-task-of-8-cities
 
 The name of what 9-letter animal can be spelled from the letters of INAUGURATION?
 """
-# This doesn't actually give the intended answer but there are some nice alternatives here
+
 import sys
 sys.path.append('..')
 from nprcommontools import get_category_members, alpha_only
