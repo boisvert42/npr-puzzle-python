9d11282ef8d4aab8ad845ffe40a88371ea38c5fa
boisvert42 <boisvert42@gmail.com>
2017-08-25 22:27:56 -0700

    improved code

--- 2017/0820_s_synonyms.py
+++ 2017/0820_s_synonyms.py
@@ -8,8 +8,11 @@ Change one of these S's to an A. You can rearrange the result to name
 a group of people, in 9 letters, that ideally have those two adjectives 
 describe them. What group is it?
 """
-
+import sys
+sys.path.append('..')
+from nprcommontools import alpha_only, sort_string, get_synonyms
 from nltk.corpus import wordnet as wn, brown
+from collections import defaultdict
 
 def is_adjective(word):
     ''' Check if the word is an adjective, per Wordnet '''
@@ -19,9 +22,18 @@ def is_adjective(word):
             return True
     return False
 
-brown_words = frozenset(x for x in brown.words() if len(x) == 5 and x.isalpha() and x.startswith('s'))
+words5 = frozenset(x for x in brown.words() if len(x) == 5 and x.isalpha() and x.startswith('s') and is_adjective(x))
+words4 = frozenset(x for x in brown.words() if len(x) == 4 and x.isalpha() and 's' in x and is_adjective(x))
+words9 = dict((sort_string(alpha_only(x)),x) for x in wn.all_lemma_names() if len(alpha_only(x)) == 9)
 #%%
-# We'll just print all five-letter adjectives beginning with S and go by inspection.
-for word in sorted(brown_words):
-    if is_adjective(word):
-        print word
+for word in sorted(words5):
+    syns = get_synonyms(word)
+    for syn in syns.intersection(words4):
+        s1 = sort_string('a' + word[1:] + syn)
+        #if s1:
+        #    print word, syn
+        try:
+            new_word = words9[s1]
+            print word, syn, new_word
+        except KeyError:
+            pass
